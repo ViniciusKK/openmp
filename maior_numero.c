5ddd47a48d65f121d4fd5a4869abfb5c9e1781a5
ViniciusKK <vinicius.kumagai@sou.inteli.edu.br>
2024-11-14 12:14:58 -0300

    add maior numero
    add maior numero

--- maior_numero.c
+++ maior_numero.c
@@ -1,11 +1,13 @@
 // Compilação:
-// gcc -fopenmp -osoma_paralela soma_paralela.c
+// gcc -fopenmp -o maior_numero maior_numero.c
 // Execução:
 // ./maior_numero
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
 #include <omp.h>
+#include <limits.h>
 
 #define N 100000000  // 100 milhões
 
@@ -19,44 +21,47 @@ int main() {
     // Inicializa o gerador de números aleatórios
     srand(time(NULL));
 
-    // Preenche o array com números aleatórios de 1 a 100
+    // Preenche o array com números aleatórios de 1 a N
     for (int i = 0; i < N; i++) {
-        array[i] = (rand() % 100) + 1;  // Gera números de 1 a 100
+        array[i] = (rand() % N) + 1;  // Gera números de 1 a N
     }
 
     double inicio, fim;
-    long long soma = 0;
+    int maior = INT_MIN;  // Usa o menor inteiro possível
 
-    // Cálculo da soma usando um loop simples com for
+    // Achando o maior usando um loop simples com for
     inicio = omp_get_wtime();
     for (int i = 0; i < N; i++) {
-        soma += array[i];
+        if (array[i] > maior) {
+            maior = array[i];
+        }
     }
     fim = omp_get_wtime();
-    printf("Soma (serial): %lld\n", soma);
+    printf("Maior número (serial): %d\n", maior);
     printf("Tempo decorrido (serial): %f segundos\n", fim - inicio);
 
-    // Cálculo da soma usando OpenMP com diferentes números de threads
+    // Achando o maior usando OpenMP com diferentes números de threads
     int num_threads[] = {2, 4, 8, 16};
     int num_configs = sizeof(num_threads) / sizeof(num_threads[0]);
 
     for (int idx = 0; idx < num_configs; idx++) {
         int n_threads = num_threads[idx];
-        soma = 0;  // Reinicia a soma
+        maior = INT_MIN;  // Reinicia o maior
         omp_set_num_threads(n_threads);
 
         inicio = omp_get_wtime();
-        #pragma omp parallel for reduction(+:soma)
+        #pragma omp parallel for reduction(max:maior)
         for (int i = 0; i < N; i++) {
-            soma += array[i];
+            if (array[i] > maior) {
+                maior = array[i];
+            }
         }
         fim = omp_get_wtime();
 
-        printf("Soma (OpenMP com %d threads): %lld\n", n_threads, soma);
+        printf("Maior (OpenMP com %d threads): %d\n", n_threads, maior);
         printf("Tempo decorrido (OpenMP com %d threads): %f segundos\n", n_threads, fim - inicio);
     }
 
     free(array);
-
-    return 0;
+return 0;
 }
